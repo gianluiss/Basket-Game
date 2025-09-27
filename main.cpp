1164c3721e1f7e50c9cdab983cda93934718b2bb
luisito0028 <gianluistapia28@gmail.com>
2025-09-27 22:20:51 +0800

    Added setupLines which randomizes the lines string

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <ncurses.h>
+
+struct Sky
+{
+    std::string lines {}; 
+    Sky* next {nullptr};
+
+    void setupSky()
+    {
+        constexpr int lineSize {20};
+        for(int line = 0; line < lineSize; line++)
+        {
+            int state = std::rand() % 3;
+
+            if(state == 0)
+                lines += ' ';
+            else if(state == 1)
+                lines += '$';
+            else if(state == 2)
+                lines += '.';
+            else
+            {
+                std::cerr << "Error. Wrong State\n";
+                return;
+            }
+        }
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World" << '\n';
+    std::srand(std::time(nullptr));
+    int r = std::rand() % 100 + 1; // 1-100
+
+    Sky* head = new Sky;
+    head->setupSky();
+
+    std::cout << "| " << head->lines << " |\n";
 
     return 0;
 }
